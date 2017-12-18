16ac22638533ec798418ef708cda641f0a0f3a24
Oliver <oliverbeatson@hotmail.co.uk>
2017-12-18 10:19:35 +0000

    Update

--- Animate.py
+++ Animate.py
@@ -16,6 +16,8 @@ import operator
 import matplotlib.pyplot
 import matplotlib.animation 
 
+# Defines Agents and iterations
+
 num_of_agents = 10
 num_of_iterations = 100
 agents = []
@@ -25,9 +27,6 @@ agents = []
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
 
-
-#ax.set_autoscale_on(False)
-
 # Make the agents.
 for i in range(num_of_agents):
     agents.append([random.randint(0,100),random.randint(0,100)])
@@ -35,9 +34,12 @@ for i in range(num_of_agents):
 carry_on = True	
 	
 def update(frame_number):
-    
+ 
     fig.clear()   
     global carry_on
+ 
+# Defines movements of agents
+# Coordinates set to random
     
     for i in range(num_of_agents):
         if random.random() < 0.5:
@@ -53,23 +55,28 @@ def update(frame_number):
     if random.random() < 0.1:
         carry_on = False
         print("stopping condition")
+        
+# Places agents within table
     
     for i in range(num_of_agents):
         matplotlib.pyplot.scatter(agents[i][0],agents[i][1])
         #print(agents[i][0],agents[i][1])
 
+
+# Defines the visual content to include in graph
 		
 def gen_function(b = [0]):
     a = 0
-    global carry_on #Not actually needed as we're not assigning, but clearer
+    global carry_on 
     while (a < 10) & (carry_on) :
         yield a			# Returns control and waits next call.
         a = a + 1
 
 
-#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
+# Creates the movement animation within the table
 animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
 
 
+# Displays the table in seperete pop-up page
 
 matplotlib.pyplot.show()
